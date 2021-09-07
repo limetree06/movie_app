3f580dcb72c2319bb2d31bd37a6f9d63e052b5ae
limetree06 <limetree06@unist.ac.kr>
2021-09-07 23:04:43 +0900

    9/7 study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from "react";
 
+function Food(props) {
+  return <h4>I love {props.name} </h4>;
+}
+
+const foodILike = [
+  { name: "111111" },
+  { name: "2" },
+  { name: "3" },
+  { name: "4" },
+  { name: "5" },
+];
+
+//main page에 들어갈 내용을 짜는 부분
+//실제 main page는 public 의 index.html
+//App.js를 index.html에 박아넣어서 실행되고 있는 것이다.
+//박아넣는것은 index.js에서 해주고 있다.
 function App() {
-  return <div className="App">Hello!!!!!!!!!</div>;
+  return (
+    <div className="App">
+      <h1>Hello!!!!!!!!!</h1>
+      {foodILike.map(
+        (
+          dish //dish is an object that from foodILike eleement
+        ) => (
+          <Food name={dish.name} />
+        )
+      )}
+    </div>
+  );
 }
 
 export default App;
+
+//<Food fav="kimchi" something={true} papapapp={[1, 2, 3, 4, 5, true]} />
+//이렇게 적으면 props object에 묶여서 데이터 보내진다.
+//react는 props 를 이용해서 데이터를 주고 받는다
+
+//js안에 html로 코딩하면 -> jsx?
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+//react can only render one component at a time
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+//component is a function that returns HTML
