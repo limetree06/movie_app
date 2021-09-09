ef50fa425e962b1bd857bcca0fb137473772a599
limetree06 <limetree06@unist.ac.kr>
2021-09-09 23:46:09 +0900

    #4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
-  state={
+class App extends React.Component {
+  state = {
     isLoading: true,
-    movie : []
+    movies: [],
   };
 
-  componentDidMount(){
-    setTimeout( () =>{
-      this.setState({isLoading : false});
-    }, 6000);
+  getMovies = async () => {
+    //axios could take a little bit of time. so we put async
+    // async this function and what do you have to wait? => axios
+    //console.log(movies.data.data.movies);
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sory_by=rating"
+    );
+    this.setState({ movies: movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
   }
 
-  render(){
-    const{ isLoading } = this.state; 
-    return (<div> { isLoading ? "Loading" : "We are ready"} </div>)
+  render() {
+    const { isLoading, movies } = this.state;
+
+    return (
+      <div>
+        {" "}
+        {isLoading
+          ? "Loading"
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}{" "}
+      </div>
+    );
   }
 }
 
